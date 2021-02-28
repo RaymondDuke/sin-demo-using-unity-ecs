c2d5a46998d10a5bd64bd929388acb65c1eaa7b9
RaymondDuke <raymondduke@gmail.com>
2021-02-28 23:01:42 +0400

    Add files via upload

---
+++ Assets/Spawner.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Mesh unitMesh;
+    [SerializeField] private Material unitMaterial;
+    [SerializeField] private GameObject gameObjectPrefab;
+
+    [SerializeField] int xSize = 10;
+    [SerializeField] int ySize = 10;
+    [Range(0.1f, 3f)]
+    [SerializeField] float spacing = 1f;
+
+
+    private Entity entityPrefab;
+    private World defaultWorld;
+    private EntityManager entityManager;
+
+    private void Start()
+    {
+        defaultWorld = World.DefaultGameObjectInjectionWorld;
+        entityManager = defaultWorld.EntityManager;
+
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(defaultWorld, null);
+        entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(gameObjectPrefab, settings);
+
+        // InstantiateEntity(new float3(-2f, 0f, 0f));
+
+        InstantiateEntityGrid(xSize, ySize, spacing);
+    }
+
+    private void InstantiateEntity(float3 position)
+    {
+        Entity myEntity = entityManager.Instantiate(entityPrefab);
+        entityManager.SetComponentData(myEntity, new Translation
+        {
+            Value = position
+        });
+    }
+
+    private void InstantiateEntityGrid(int dimX, int dimY, float spacing = 1f)
+    {
+        for (int i = 0; i < dimX; i++)
+        {
+            for (int j = 0; j < dimY; j++)
+            {
+                InstantiateEntity(new float3(i * spacing, j * spacing, 0f));
+            }
+        }
+    }
+
+
+    private void MakeEntity()
+    {
+        // 1 define an EntityArchetype
+
+        EntityArchetype archetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            );
+
+        // 2 create an Entity
+
+        Entity myEntity = entityManager.CreateEntity(archetype);
+
+        // 3 add data to Entity
+
+        entityManager.AddComponentData(myEntity, new Translation
+        {
+            Value = new float3(0f, 0f, 0f)
+        });
+
+        entityManager.AddSharedComponentData(myEntity, new RenderMesh
+        {
+            mesh = unitMesh,
+            material = unitMaterial,
+        });
+
+    }
+
+}
